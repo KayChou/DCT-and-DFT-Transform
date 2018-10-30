55f5cc70ae6d73175140901f7a0a8bc78e56fd92
xiaomi <gyxxzk@gmail.com>
2018-10-30 15:25:07 +0800

    add IDCT and IDST

--- DCT_DFT_transform.m
+++ DCT_DFT_transform.m
@@ -6,13 +6,21 @@ title("Origin Image");
 
 % DCT Transform
 img_dct = DCT_2D(img);
-subplot(1, 3, 2);
+subplot(2, 3, 2);
 imshow(img_dct)
 title("DCT Transform")
+subplot(2, 3, 5)
+img_idct = IDCT_2D(img_dct);
+imshow(img_idct);
+title("IDCT Transform")
 
 % DST Transform
 %img_dst = dst(img);
 img_dst = DST_2D(img);
-subplot(1, 3, 3);
+subplot(2, 3, 3);
 imshow(img_dst);
 title("DST Transform");
+subplot(2, 3, 6)
+img_idst = IDST_2D(img_dst);
+imshow(img_idst)
+title("IDST Transform")
---
+++ IDST_2D.m
@@ -0,0 +1,16 @@
+function [output] = IDST_2D(input)
+%DST Transform
+%input is an N by N matrix
+%output is the dct transform of input
+shape = size(input);
+N = shape(1);
+matrix = zeros(N);
+for i=1:N
+    c = 2 / sqrt(2*N+1);
+    for j=1:N
+        matrix(i, j) = c * sin( pi*j*i / (N+1) );
+    end
+end
+input = double(input);
+output = matrix' * input * matrix / N;
+end
